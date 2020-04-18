86a9a3d23b5bcce72bde79998050b6b99cc2d436
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-18 06:46:04 +0300

    Project Initialization

---
+++ rentalApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bwm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rentalApp';
+}
