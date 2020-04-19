f9e4879881fc9bec73be65f40c029d9301dd9d16
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-20 02:05:53 +0300

    Rental Module

--- rentalApp/src/app/app.module.ts
+++ rentalApp/src/app/app.module.ts
@@ -4,20 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { RentalDetailComponent } from './rental/rental-detail/rental-detail.component';
-import { RentalListingComponent } from './rental/rental-listing/rental-listing.component';
+
+import { RentalModule } from './rental/rental.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RentalDetailComponent,
-    RentalListingComponent
-
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rentalApp/src/app/rental/rental.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rental',
+  templateUrl: './rental.component.html'
+})
+export class RentalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
