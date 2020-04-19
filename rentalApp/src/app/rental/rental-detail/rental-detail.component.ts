50dbaf1c637bfb52ac117ead232c29bddb04c223
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-20 02:32:09 +0300

    get routing params

--- rentalApp/src/app/rental/rental-detail/rental-detail.component.ts
+++ rentalApp/src/app/rental/rental-detail/rental-detail.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-rental-detail',
   templateUrl: './rental-detail.component.html',
   styleUrls: ['./rental-detail.component.css']
 })
 export class RentalDetailComponent implements OnInit {
-
-  constructor() { }
+rentalId = '';
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params =>{
+      this.rentalId = params['rentalId'];
+    })
   }
 
 }
