98d50e8259bc8b8872d6d19c7a9ce24ac30d4cfe
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-21 06:04:21 +0300

    Generic Interface

--- rentalApp/src/app/rental/rental-listing/rental-listing.component.ts
+++ rentalApp/src/app/rental/rental-listing/rental-listing.component.ts
@@ -2,20 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { Rental } from '../shared/rental.model'
 import { RentalService } from '../shared/rental.service';
 
+import { IRental, AppStorage } from "../shared/rental.interface";
+
 @Component({
   selector: 'app-rental-listing',
   templateUrl: './rental-listing.component.html',
   styleUrls: ['./rental-listing.component.css']
 })
-export class RentalListingComponent implements OnInit {
+export class RentalListingComponent implements OnInit, IRental {
   rentals: Rental[] = [];
+
+  someData = 'some data';
+  isLoaded = false;
 
   constructor(private rentalService: RentalService) { }
 
   ngOnInit() {
-   this.rentalService.getRentals().subscribe((rentals: Rental[]) => {
-    this.rentals = rentals;
-   });
+    const appStorange = new AppStorage<number>();
+    
+    this.rentalService.getRentals().subscribe((rentals: Rental[]) => {
+      this.rentals = rentals;
+    });
+  }
+
+  implementMe() {
+    return '';
   }
 
 }
