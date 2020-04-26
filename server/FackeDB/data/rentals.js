02021472d5132525b13887b949b8223b15631480
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-26 06:27:31 +0300

    Fake DB

---
+++ server/FackeDB/data/rentals.js
@@ -0,0 +1,50 @@
+
+const rentals = [
+    {
+        title: 'central Apartment',
+        city: 'new yourk',
+        street: 'times square',
+        category: 'apartment',
+        image: 'http://via.placeholder.com/350x250',
+        numOfRooms: 3,
+        description: 'very nice apartment',
+        dailyPrice: 34,
+        shared: false
+      },
+      {
+        title: 'Central Apartment 2',
+        city: "san francisco",
+        street: "Main street",
+        category: "condo",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 2,
+        description: "Very nice apartment",
+        dailyPrice: 12,
+        shared: true
+      },
+      {
+        title: "Central Apartment 3",
+        city: "bratislava",
+        street: "Hlavna",
+        category: "condo",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 2,
+        description: "Very nice apartment",
+        dailyPrice: 334,
+        shared: true
+      },
+      {
+        title: "Central Apartment 4",
+        city: "berlin",
+        street: "Haupt strasse",
+        category: "house",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 9,
+        description: "Very nice apartment",
+        dailyPrice: 33,
+        shared: true
+      }
+]
+
+
+module.exports = rentals;
---
+++ server/resources/1_rentals_data.js
@@ -0,0 +1,55 @@
+[
+    {
+        _id: '1',
+        title: 'central Apartment',
+        city: 'new yourk',
+        street: 'times square',
+        category: 'apartment',
+        image: 'http://via.placeholder.com/350x250',
+        numOfRooms: 3,
+        description: 'very nice apartment',
+        dailyPrice: 34,
+        shared: false,
+        createdAt: '1/1/2020'
+      },
+      {
+        _id: '2',
+        title: 'Central Apartment 2',
+        city: "san francisco",
+        street: "Main street",
+        category: "condo",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 2,
+        description: "Very nice apartment",
+        dailyPrice: 12,
+        shared: true,
+        createdAt: "24/12/2017"
+      },
+      {
+        _id: "3",
+        title: "Central Apartment 3",
+        city: "bratislava",
+        street: "Hlavna",
+        category: "condo",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 2,
+        description: "Very nice apartment",
+        dailyPrice: 334,
+        shared: true,
+        createdAt: "24/12/2017"
+      },
+      {
+        _id: "4",
+        title: "Central Apartment 4",
+        city: "berlin",
+        street: "Haupt strasse",
+        category: "house",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 9,
+        description: "Very nice apartment",
+        dailyPrice: 33,
+        shared: true,
+        createdAt: "24/12/2017"
+      }
+]
+   
