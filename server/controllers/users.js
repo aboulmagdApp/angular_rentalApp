75908c800529899159d91237f7ca8b3075428f9e
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-02 01:26:46 +0300

    Register User

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,10 +1,36 @@
 
-
+const User = require('../models/user');
 
 exports.login = (req, res) => {
     return res.json({ message: 'Logging in' })
 }
 
 exports.register = (req, res) => {
-    return res.json({ message: 'Registring is done!' })
+    // this will conme from any client side app 
+    const { username, email, password, passwordConfirmation } = req.body;
+    if (!password || !email) {
+        return res.status(422).send({ errors: [{ title: 'Missing Data', detail: 'Email or password is missing!' }] });
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.status(422).send({ errors: [{ title: 'Invalid password', detail: 'Password is not maching confirmation password!' }] });
+    }
+
+    User.findOne({ email }, (error, existingUser) => {
+        if (error) {
+            return res.status(422).send({ errors: [{ title: 'DB Error', detail: 'Oooops, something went wrong!' }] });
+        }
+
+        if (existingUser) {
+            return res.status(422).send({ errors: [{ title: 'Invalid Email', detail: 'User with provided email already exists!' }] });
+        }
+        const user = new User({ username, email, password });
+        user.save((error) => {
+            if (error) {
+                return res.status(422).send({ errors: [{ title: 'DB Error', detail: 'Oooops, something went wrong!' }] });
+            }
+            
+            return res.json({ message: 'Registring is done!' })
+        })
+    })
 }
