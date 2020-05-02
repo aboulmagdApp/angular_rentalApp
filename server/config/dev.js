86cda52d50e6c1a1b85d6538e887a80ea773516e
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-02 04:05:52 +0300

    JWT Intro

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,6 @@
 
 
 module.exports = {
-    DB_URI : 'mongodb://aboulmagd:aqtkOcbhYbQD0biM@cluster0-shard-00-00-vjxvu.mongodb.net:27017,cluster0-shard-00-01-vjxvu.mongodb.net:27017,cluster0-shard-00-02-vjxvu.mongodb.net:27017/bwmNewDev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    DB_URI : 'mongodb://aboulmagd:aqtkOcbhYbQD0biM@cluster0-shard-00-00-vjxvu.mongodb.net:27017,cluster0-shard-00-01-vjxvu.mongodb.net:27017,cluster0-shard-00-02-vjxvu.mongodb.net:27017/bwmNewDev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    JWT_SECRET: 'sdsa545?sad!'
 }
