157d94136a14df8f6783d7d909d7dcbfa608734b
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-11 05:13:03 +0300

    Save JWT

--- rentalApp/src/app/auth/shared/auth.service.ts
+++ rentalApp/src/app/auth/shared/auth.service.ts
@@ -4,13 +4,25 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { extractApiError } from 'src/app/shared/helpers/functions';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+const jwt = new JwtHelperService();
+
+class DecodedToken {
+  exp: number = 0;
+  username: string = ''
+  userId = ''
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+private decodedToken: DecodedToken;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.decodedToken = new DecodedToken;
+  }
 
   register(formData: RegisterForm): Observable<any> {
     return this.http
@@ -25,7 +37,7 @@ export class AuthService {
       .post('/api/v1/users/login', formData)
       .pipe(
         map((token: string) =>{
-          this.saveToken(token);
+         this.saveToken(token);
           return token;
         }),
         catchError((resError: HttpErrorResponse) =>
@@ -33,7 +45,11 @@ export class AuthService {
       )
     )}
 
-    private saveToken(token){
-      alert('I am saving token!');
+    private saveToken(token: string): string | null{
+      const decodedToken = jwt.decodeToken(token);
+      if(!decodedToken){ return null; }
+      this.decodedToken = decodedToken;
+      localStorage.setItem('bwm_auth_token', token);
+      return token;
     }
 }
