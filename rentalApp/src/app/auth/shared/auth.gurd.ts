d49fc01a6d9122bc5234b62f05e97866b1ccbd1e
Mohamed Aboulmagd <aboulmagd@live.com>
2020-05-12 17:31:12 +0300

    Finishing auth guard

--- rentalApp/src/app/auth/shared/auth.gurd.ts
+++ rentalApp/src/app/auth/shared/auth.gurd.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router'
+import { AuthService } from './auth.service';
 
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class AuthGuard implements CanActivate{
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-       return false
+export class AuthGuard implements CanActivate {
+    private url;
+
+    constructor(private auth: AuthService, private router: Router) {
+    }
+
+    canActivate(_: any, state: RouterStateSnapshot) {
+        this.url = state.url;
+
+        return this.auth.isAuthenticated ?
+            this.handleAuthState() : this.handleNotAuthState();
+    }
+
+    private handleAuthState(): boolean {
+        if (this.isAuthPage) {
+            this.router.navigate(['/rentals']);
+            return false;
+        }
+
+        return true;
+    }
+
+    private handleNotAuthState(): boolean {
+        if (this.isAuthPage) {
+            return true;
+        }
+        this.router.navigate(['/login']);
+        return false;
+    }
+
+    private get isAuthPage(): boolean {
+        if (this.url.includes('login') || this.url.includes('register')) {
+            return true;
+        }
+
+        return false;
     }
 
 }
